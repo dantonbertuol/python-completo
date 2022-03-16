030d38aa0ac67686d493fd7107fa317e17c14c49
Danton Bertuol <dantonjb@gmail.com>
2022-03-15 21:45:24 -0300

    Reforço sobre getter e setter, estudo de associacao, agregacao e composicao.

---
+++ atividades-aulas/107-associacao.py
@@ -0,0 +1,43 @@
+class Escritor:
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__ferramenta = None
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def ferramenta(self):
+        return self.__ferramenta
+
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self.__ferramenta = ferramenta
+
+
+class Caneta:
+    def __init__(self, marca):
+        self.__marca = marca
+
+    @property
+    def marca(self):
+        return self.__marca
+
+    def escrever(self):
+        print('Caneta está escrevendo...')
+
+class MaquinaDeEscrever:
+    def escrever(self):
+        print('Máquina está escrevendo...')
+
+escritor = Escritor('Joãozinho')
+caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+
+escritor.ferramenta = maquina # Atribui o objeto maquina para a ferramenta do escrito
+escritor.ferramenta.escrever() # Chama o método da maquina que esta no atributo ferramenta do escritor
+
+del escritor # Se remover o escrito a caneta e a maquina ainda continuam existindo
+print(caneta.marca)
+maquina.escrever()
