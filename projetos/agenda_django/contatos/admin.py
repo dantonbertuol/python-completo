ea2f90b5037aa97c7b8219f374771a9d522e6814
Danton Bertuol <dantonjb@gmail.com>
2022-05-02 21:29:49 -0300

    Estudo da area administrativa do django, exibindo tabelas, adicionado filtros, editando colunas para serem exibidas. Estudo de levantamento de erros 404. Estudo de exibicao de um unico contato passando por parametro na URL o id do contato selecionado.

--- projetos/agenda_django/contatos/admin.py
+++ projetos/agenda_django/contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria') # Colunas que serão exibidas
+    list_display_links = ('id', 'nome', 'sobrenome') # Colunas que terão link para acessar o contato
+    #list_filter = ('nome', 'sobrenome') # Adiciona opções de filtragem
+    list_per_page = 10 # Exibe 10 elementos por página
+    search_fields = ('nome', 'sobrenome', 'telefone') # Insere campo de pesquisa por esses itens
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- projetos/agenda_django/contatos/models.py
+++ projetos/agenda_django/contatos/models.py
@@ -4,6 +4,10 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255) # Campo tipo string com tamanho máximo 255
 
+    # Será exibido no admin do django
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=255) # Campo tipo string com tamanho máximo 255
     sobrenome = models.CharField(max_length=255, blank=True) # Campo tipo string com tamanho máximo 255, opcional (pode ser blank)
@@ -12,3 +16,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now) # Campo tipo DateTime com o valor default
     descricao = models.TextField(blank=True) # Campo tipo Texto
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
