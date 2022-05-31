56d15ef328d711642408a050b5439186684e1c2a
Danton Bertuol <danton.bertuol@unoesc.edu.br>
2022-05-31 11:15:47 -0300

    Criada página inicial de conteúdo dos posts, com paginação, links, de forma dinamica.

--- projetos/blog_django/blog/settings.py
+++ projetos/blog_django/blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static', )
+STATICFILES_DIRS = (BASE_DIR / 'templates/static', )
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
--- projetos/blog_django/posts/views.py
+++ projetos/blog_django/posts/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from posts.models import Post
 
 # Modelo Class Based Views - Com esse modelo pode herdar das classes
 class PostIndex(ListView):
-    ...
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 1
+    context_object_name = 'posts'
 
 class PostBusca(PostIndex):
     ...
