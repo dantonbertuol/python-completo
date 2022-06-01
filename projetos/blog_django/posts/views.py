1ce74eb28663dfb49bbe9de506af7cdc0dbfa89f
Danton Bertuol <dantonjb@gmail.com>
2022-05-31 21:34:44 -0300

    Tratado para somente exibir comentarios e posts publicados, implementado campo de busca, implementada navegação entre categorias de posts, alterada ordenação de exibição dos posts

--- projetos/blog_django/posts/views.py
+++ projetos/blog_django/posts/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 
 # Modelo Class Based Views - Com esse modelo pode herdar das classes
 class PostIndex(ListView):
@@ -9,11 +10,55 @@ class PostIndex(ListView):
     paginate_by = 1
     context_object_name = 'posts'
 
+    # Manipula a query padrão 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publicado_post = True) #ordena pelo id de forma descrescente e filtra os posts publicados
+        qs = qs.annotate(
+            numero_comentarios = Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+
+        return qs
+
 class PostBusca(PostIndex):
-    ...
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+
+        return qs
 
 class PostCategoria(PostIndex):
-    ...
+    template_name = 'posts/post_categoria.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
 
 class PostDetalhes(UpdateView):
     ...
