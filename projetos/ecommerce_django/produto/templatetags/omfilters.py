7a4657825742195a8f856d19f19972a821f4b20b
Danton Bertuol <dantonjb@gmail.com>
2022-06-21 21:34:41 -0300

    Implementada listagem de produtos, imagens, descricao, paginação e links para detalhamento do produto de forma dinamica.

---
+++ projetos/ecommerce_django/produto/templatetags/omfilters.py
@@ -0,0 +1,10 @@
+from ctypes import util
+from django.template import Library
+from utils import utils
+
+register = Library()
+
+
+@register.filter
+def formata_preco(valor):
+    return utils.formata_preco(valor)
