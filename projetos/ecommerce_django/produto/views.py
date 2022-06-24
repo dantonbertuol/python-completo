1201155141faf983cd60469748288bc516d8dd37
Danton Bertuol <dantonjb@gmail.com>
2022-06-23 21:42:19 -0300

    Implementada pagina de detalhamento do produto. Iniciada implementacao do carrinho de compras utilizando sessoes.

--- projetos/ecommerce_django/produto/views.py
+++ projetos/ecommerce_django/produto/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -11,14 +14,50 @@ class ListaProdutos(ListView):
     paginate_by = 2
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('DetalheProduto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
+    # Utiliza get pois o formulario eh get se nao seria post o metodo
     def get(self, *args, **kwargs):
-        return HttpResponse('AdicionarAoCarrinho')
+        # Url de onde o cliente veio HTTP_REFERER
+        http_referer = self.request.META.get(
+            'HTTP_REFERER', reverse('produto:lista'))
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não existe'
+            )
+            # Direciona o cliente de onde ele veio com o metodo HTTP_REFERER
+            return redirect(http_referer)
+
+        variacao = get_object_or_404(models.Variacao, id=variacao_id)
+
+        # Verifica se tem a chave carrinho na sessão
+        if not self.request.session.get('carrinho'):
+            # Se não existir cria com o nome carrinho (é um dicionario)
+            self.request.session['carrinho'] = {}
+            # Salva a sessão
+            self.request.session.save()
+
+        # Dicionario da sessao salvo na variavel carrinho
+        carrinho = self.request.session['carrinho']
+
+        # Se a variação já existe no carrinho
+        if variacao_id in carrinho:
+            # TODO: variação existe no carrinho
+            pass
+        else:
+            # TODO: variação não existe no carrinho
+            pass
+
+        return HttpResponse(f'{variacao.produto} {variacao.nome} ')
 
 
 class RemoverDoCarrinho(View):
